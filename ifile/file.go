1c68d512df7487367d4369d211d0f55846842d88
zanjs <root@zanjs.com>
2018-02-22 16:27:50 +0800

    mv mkdir fn

--- ifile/file.go
+++ ifile/file.go
@@ -14,3 +14,21 @@ func CheckFileIsExist(filename string) bool {
 	}
 	return exist
 }
+
+// Mkdir is
+func Mkdir(name string) {
+	var path string
+	if os.IsPathSeparator('\\') { //前边的判断是否是系统的分隔符
+		path = "\\"
+	} else {
+		path = "/"
+	}
+	fmt.Println(path)
+	dir, _ := os.Getwd()                        //当前的目录
+	err := os.Mkdir(dir+path+name, os.ModePerm) //在当前目录下生成目录
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("mkdir " + dir + path + name)
+	}
+}
